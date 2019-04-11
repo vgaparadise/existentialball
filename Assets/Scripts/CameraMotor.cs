999a9ad47bb6688b2bd274763fe88f08da3244f9
vgaparadise <hoyogo1@gmail.com>
2019-04-11 13:09:14 -0400

    the ball is now rolling

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+    //what the camera will be focused on
+    public GameObject target;
+    public Vector3 distanceFromPlayer;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = target.transform.position + distanceFromPlayer;
+        //point the camera at the player
+        transform.LookAt(target.transform.position);
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float playerSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get both the axis of control 
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        //grab hold of the body
+        Rigidbody body = GetComponent<Rigidbody>();
+        //Multiply speed times time to be framerate independent
+        body.AddTorque(new Vector3(xAxis, 0, yAxis) * playerSpeed * Time.deltaTime);
+
+
+	}
+}
