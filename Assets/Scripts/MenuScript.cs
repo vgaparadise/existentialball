0f8491b0edefe97f720225c3f96a1fd3a21e60be
vgaparadise <hoyogo1@gmail.com>
2019-05-01 00:53:33 -0400

    aestetic

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MenuScript : MonoBehaviour
 {
-    public Button B;
-    public Button A;
     public Button DEBUG_START;
     public string force_level_load;
     // Start is called before the first frame update
     void Start()
     {
         this.DEBUG_START = DEBUG_START;
-        this.B = B;
-        this.A = A;
-        B.onClick.AddListener(BOnClick);
-        A.onClick.AddListener(AOnClick);
         DEBUG_START.onClick.AddListener(DOnClick);
     }
     void DOnClick()
@@ -23,16 +17,6 @@ public class MenuScript : MonoBehaviour
         GameManager.StartGame(force_level_load);
     }
 
-
-    void BOnClick()
-    {
-        GameManager.StartGame("B1");
-    }
-
-    void AOnClick()
-    {
-        GameManager.StartGame("A1");
-    }
     // Update is called once per frame
     void Update()
     {
